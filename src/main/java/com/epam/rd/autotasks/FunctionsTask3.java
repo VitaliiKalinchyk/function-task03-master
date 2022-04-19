3f4a6d07e883a733e712f91fb80f25a7d2db81f4
mmveres <mmveres@gmail.com>
2022-04-19 15:25:34 +0300

    function-task3-master

---
+++ src/main/java/com/epam/rd/autotasks/FunctionsTask3.java
@@ -0,0 +1,15 @@
+package com.epam.rd.autotasks;
+
+public class FunctionsTask3 {
+    /**
+     * <summary>
+     * Implement code according to description of task.
+     * </summary>
+     * if set invalid arguments in method, then method must throws
+     * IllegalArgumentException
+     */
+    public static int multiArithmeticElements(int a1, int t, int n) {
+        //TODO: Delete line below and write your own solution
+        throw new UnsupportedOperationException();
+    }
+}
